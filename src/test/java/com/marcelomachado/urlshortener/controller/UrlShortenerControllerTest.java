b34fc22663363eb76960fcddc216c5133ca304f1
MarceloMachadoxD <marcelo.ultraje@gmail.com>
2024-09-20 20:02:01 -0300

    Adjust ControllerTests add Mockito and remove deprecated getStatusCodeValue methods

--- src/test/java/com/marcelomachado/urlshortener/controller/UrlShortenerControllerTest.java
+++ src/test/java/com/marcelomachado/urlshortener/controller/UrlShortenerControllerTest.java
@@ -1,8 +1,12 @@
 package com.marcelomachado.urlshortener.controller;
 
+import com.marcelomachado.urlshortener.service.UrlShortenerService;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.ResponseEntity;
@@ -13,19 +17,27 @@ class urlShortenerControllerTests {
     private int BAD_REQUEST = 400;
     private int CREATED = 201;
 
+    @Mock
+    private UrlShortenerService urlShortenerService;
 
     @Autowired
     private UrlShortenerController urlShortenerController;
+
+    @BeforeEach
+    void setup() {
+        urlShortenerController = new UrlShortenerController(urlShortenerService);
+    }
 
     @Test
     @DisplayName("When receive a valid url should return a response with CREATED status")
     void postUrl_shouldReturnUrl() {
         String url = "https://www.marcelomachado.com";
+        Mockito.when(urlShortenerService.shortenUrl(Mockito.any())).thenReturn("index/hash");
 
         ResponseEntity<String> response = urlShortenerController.postUrl(url);
 
-        Assertions.assertEquals(CREATED, response.getStatusCodeValue());
-        Assertions.assertEquals(url, response.getBody());
+        Assertions.assertEquals(CREATED, response.getStatusCode().value());
+        Assertions.assertEquals("index/hash", response.getBody());
     }
 
     @Test
@@ -35,7 +47,7 @@ class urlShortenerControllerTests {
 
         ResponseEntity<String> response = urlShortenerController.postUrl(url);
 
-        Assertions.assertEquals(BAD_REQUEST, response.getStatusCodeValue());
+        Assertions.assertEquals(BAD_REQUEST, response.getStatusCode().value());
     }
 
     @Test
@@ -45,7 +57,7 @@ class urlShortenerControllerTests {
 
         ResponseEntity<String> response = urlShortenerController.postUrl(url);
 
-        Assertions.assertEquals(BAD_REQUEST, response.getStatusCodeValue());
+        Assertions.assertEquals(BAD_REQUEST, response.getStatusCode().value());
     }
 
 }
