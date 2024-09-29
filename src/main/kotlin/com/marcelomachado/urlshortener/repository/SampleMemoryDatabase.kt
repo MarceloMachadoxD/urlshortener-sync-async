de07c6a419ced301067d8e1830a989a5cbfb31e6
MarceloMachadoxD <marcelo.ultraje@gmail.com>
2024-09-29 17:21:34 -0300

    update md5 generator to utils

--- src/main/kotlin/com/marcelomachado/urlshortener/repository/SampleMemoryDatabase.kt
+++ src/main/kotlin/com/marcelomachado/urlshortener/repository/SampleMemoryDatabase.kt
@@ -1,19 +1,16 @@
 package com.marcelomachado.urlshortener.repository
 
 import com.marcelomachado.urlshortener.entity.UrlShort
+import com.marcelomachado.urlshortener.util.Utils
 import org.springframework.stereotype.Component
 import reactor.core.publisher.Mono
-import java.math.BigInteger
-import java.nio.charset.StandardCharsets
-import java.security.MessageDigest
-import java.security.NoSuchAlgorithmException
 
 @Component
 class SampleMemoryDataBase : Database {
     private val urlShortMap = LinkedHashMap<String?, String?>()
 
     override fun saveUrl(url: String): Mono<UrlShort> {
-        urlShortMap.put(url, md5(url))
+        urlShortMap.put(url, Utils.md5(url))
         val index = urlShortMap.keys.indexOf(url).toLong()
 
         val persistedHashedValue = urlShortMap.get(url).toString()
@@ -24,21 +21,6 @@ class SampleMemoryDataBase : Database {
 
     override fun getUrl(url: String): Mono<String> {
         TODO("Not yet implemented")
-    }
-
-    private fun md5(input: String): String {
-        try {
-            val md = MessageDigest.getInstance("MD5")
-            val messageDigest = md.digest(input.toByteArray(StandardCharsets.UTF_8))
-            val no = BigInteger(1, messageDigest)
-            var hashText = no.toString(16)
-            while (hashText.length < 32) {
-                hashText = "0$hashText"
-            }
-            return hashText
-        } catch (e: NoSuchAlgorithmException) {
-            throw RuntimeException(e)
-        }
     }
 
 }
