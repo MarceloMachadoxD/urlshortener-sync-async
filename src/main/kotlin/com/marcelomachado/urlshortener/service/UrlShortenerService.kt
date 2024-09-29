7349851e50f01ddcfb2a5295a59463928cd229df
MarceloMachadoxD <marcelo.ultraje@gmail.com>
2024-09-29 19:31:27 -0300

    add logic to save on both databases Redis and Postgres

--- src/main/kotlin/com/marcelomachado/urlshortener/service/UrlShortenerService.kt
+++ src/main/kotlin/com/marcelomachado/urlshortener/service/UrlShortenerService.kt
@@ -1,6 +1,9 @@
 package com.marcelomachado.urlshortener.service
 
+import com.marcelomachado.urlshortener.entity.UrlShort
 import com.marcelomachado.urlshortener.repository.Database
+import com.marcelomachado.urlshortener.repository.UrlShortRepository
+import com.marcelomachado.urlshortener.util.Utils
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.beans.factory.annotation.Value
@@ -14,11 +17,26 @@ class UrlShortenerService {
     @Qualifier("redisDatabase")
     private lateinit var database: Database
 
+    @Autowired
+    private lateinit var urlShortRepository: UrlShortRepository
+
     @Value("\${app.base-url}")
     private lateinit var baseUrl: String
 
     fun shortenUrl(url: String): Mono<String> {
-        return database.saveUrl(url)
-            .map { baseUrl.plus(it?.hashedUrl) }
+        val hashedUrl = Utils.md5(url)
+        val urlShort = UrlShort(null, url, hashedUrl)
+
+        return urlShortRepository.save(urlShort)
+            .doOnSuccess { println("Saved URL on Database: $urlShort") }
+            .flatMap { savedUrlShort ->
+                database.saveUrl(url)
+                    .doOnSuccess { println("Saved URL on Redis: $url") }
+                    .map { baseUrl + savedUrlShort.hashedUrl }
+            }
+            .doOnError { error ->
+                println("Error on salve URL: ${error.message}")
+            }
     }
+
 }
