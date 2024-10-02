1cbe7ef9b900d99ae52585a5dcbb067d5043058a
MarceloMachadoxD <marcelo.ultraje@gmail.com>
2024-10-02 07:22:04 -0300

    javadoc

--- src/main/kotlin/com/marcelomachado/urlshortener/repository/RedisDatabase.kt
+++ src/main/kotlin/com/marcelomachado/urlshortener/repository/RedisDatabase.kt
@@ -5,7 +5,22 @@ import reactor.core.publisher.Mono
 
 interface RedisDatabase {
 
+    /**
+     * Save a URL on Redis using the hashed URL as key and the original URL as value
+     * it also configures the TTL of the key as defaultTtl setup in application.properties
+     *
+     * @param url URL to be saved
+     *
+     * @return Mono<UrlShort> with the saved hashed URL
+     */
     fun saveUrl(url: UrlShort): Mono<UrlShort>
 
-    fun getUrl(url: String): Mono<String>
+    /**
+     * Get the original URL from Redis using the hashed URL
+     *
+     * @param hashed hashed URL
+     *
+     * @return Mono<String> with the original URL
+     */
+    fun getUrl(hashed: String): Mono<String>
 }
